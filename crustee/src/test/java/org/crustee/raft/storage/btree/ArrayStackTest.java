2f54881022f7e89315ae4623c5fa9e6a69900b77
olivier bourgain <olivierbourgain02@gmail.com>
2015-08-03 00:24:48 +0200

    cleanup code

--- crustee/src/test/java/org/crustee/raft/storage/btree/ArrayStackTest.java
+++ crustee/src/test/java/org/crustee/raft/storage/btree/ArrayStackTest.java
@@ -8,7 +8,7 @@ public class ArrayStackTest {
 
     @Test
     public void should_push() throws Exception {
-        ArrayStack stack = new ArrayStack(10);
+        ArrayStack<String> stack = new ArrayStack<>(10);
         stack.push("foo");
         assertThat(stack.array).has(Utils.numberOfElementsAndNulls(1));
         assertThat(stack.array[0]).isEqualTo("foo");
@@ -17,7 +17,7 @@ public class ArrayStackTest {
 
     @Test
     public void should_pop() throws Exception {
-        ArrayStack stack = new ArrayStack(10);
+        ArrayStack<String> stack = new ArrayStack<>(10);
         stack.push("foo");
 
         Object value = stack.pop();
@@ -28,15 +28,17 @@ public class ArrayStackTest {
 
     @Test
     public void should_throw_when_popping_empty_stack() throws Exception {
-        ArrayStack stack = new ArrayStack(10);
+        ArrayStack<String> stack = new ArrayStack<>(10);
         Throwable throwable = ThrowableAssert.catchThrowable(stack::pop);
         assertThat(throwable).isInstanceOf(ArrayIndexOutOfBoundsException.class);
     }
 
     @Test
     public void should_resize_when_pushing_on_full_stack() throws Exception {
-        ArrayStack stack = new ArrayStack(1);
+        ArrayStack<String> stack = new ArrayStack<>(1);
         stack.push("foo");
         stack.push("foo");
+
+        assertThat(stack.nextIndex).isEqualTo(2);
     }
 }
