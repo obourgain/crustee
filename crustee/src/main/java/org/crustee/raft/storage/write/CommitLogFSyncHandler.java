890291ce5d95942c87f6e0938d4c871a47251fdc
olivier bourgain <olivierbourgain02@gmail.com>
2015-09-26 11:46:38 +0200

    Fix commit log fsync heuristics

--- crustee/src/main/java/org/crustee/raft/storage/write/CommitLogFSyncHandler.java
+++ crustee/src/main/java/org/crustee/raft/storage/write/CommitLogFSyncHandler.java
@@ -1,26 +1,22 @@
 package org.crustee.raft.storage.write;
 
-import static org.slf4j.LoggerFactory.getLogger;
 import org.crustee.raft.storage.commitlog.CommitLog;
 import org.crustee.raft.storage.commitlog.Segment;
-import org.slf4j.Logger;
 import com.lmax.disruptor.EventHandler;
 
 public class CommitLogFSyncHandler implements EventHandler<WriteEvent> {
 
-    private static final Logger logger = getLogger(CommitLogFSyncHandler.class);
-
     private final CommitLog commitLog;
 
-    private final int maxEvents = 1000;
-
     private final int maxUnsyncedSizeInBytes;
+    private final int maxUnsyncedCount;
     private int eventCount = 0;
     private int unsyncedSizeInBytes = 0;
 
     public CommitLogFSyncHandler(CommitLog commitLog, int maxUncommitedSize, int maxUnsyncedCount) {
         this.commitLog = commitLog;
         this.maxUnsyncedSizeInBytes = maxUncommitedSize;
+        this.maxUnsyncedCount = maxUnsyncedCount;
     }
 
     @Override
@@ -32,8 +28,10 @@ public class CommitLogFSyncHandler implements EventHandler<WriteEvent> {
         while ((old = commitLog.getOldSegments().poll()) != null) {
             old.sync();
         }
-        if(endOfBatch || eventCount >= maxEvents || unsyncedSizeInBytes >= maxUnsyncedSizeInBytes) {
+        if(endOfBatch || eventCount >= maxUnsyncedCount || unsyncedSizeInBytes >= maxUnsyncedSizeInBytes) {
             commitLog.getCurrent().sync();
+            eventCount = 0;
+            unsyncedSizeInBytes = 0;
         }
     }
 
