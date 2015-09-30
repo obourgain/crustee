5c5aafce139e9c945fc04ea8adb70454159be6cd
olivier bourgain <olivierbourgain02@gmail.com>
2015-09-30 07:03:49 +0200

    Get current commit log segment from event

--- crustee/src/main/java/org/crustee/raft/storage/write/CommitLogFSyncHandler.java
+++ crustee/src/main/java/org/crustee/raft/storage/write/CommitLogFSyncHandler.java
@@ -7,6 +7,7 @@ import com.lmax.disruptor.EventHandler;
 public class CommitLogFSyncHandler implements EventHandler<WriteEvent> {
 
     private final CommitLog commitLog;
+    private Segment current;
 
     private final int maxUnsyncedSizeInBytes;
     private final int maxUnsyncedCount;
@@ -15,12 +16,16 @@ public class CommitLogFSyncHandler implements EventHandler<WriteEvent> {
 
     public CommitLogFSyncHandler(CommitLog commitLog, int maxUncommitedSize, int maxUnsyncedCount) {
         this.commitLog = commitLog;
+        this.current = commitLog.getCurrentSegment();
         this.maxUnsyncedSizeInBytes = maxUncommitedSize;
         this.maxUnsyncedCount = maxUnsyncedCount;
     }
 
     @Override
     public void onEvent(WriteEvent event, long sequence, boolean endOfBatch) throws Exception {
+        if(event.getSegment() != null) {
+            current = event.getSegment();
+        }
         eventCount++;
         unsyncedSizeInBytes += event.getRowKey().limit(); // + event.getValues().limit(); TODO log the value
         syncOldSegments();
@@ -28,15 +33,17 @@ public class CommitLogFSyncHandler implements EventHandler<WriteEvent> {
     }
 
     private void syncCurrentIfNeeded(boolean endOfBatch) {
-        if (endOfBatch || eventCount >= maxEvents || unsyncedSizeInBytes >= maxUnsyncedSizeInBytes || eventCount >= maxUnsyncedCount) {
-            commitLog.syncCurrent();
+        if (endOfBatch || unsyncedSizeInBytes >= maxUnsyncedSizeInBytes || eventCount >= maxUnsyncedCount) {
+            current.sync();
+            eventCount = 0;
+            unsyncedSizeInBytes = 0;
         }
     }
 
     private void syncOldSegments() {
         Segment old;
         while ((old = commitLog.getOldSegments().poll()) != null) {
-            old.sync();
+            unsyncedSizeInBytes -= old.sync();
         }
     }
 
